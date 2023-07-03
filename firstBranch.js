d33ba052d0673481665c37309df4f9d054506400
Boluwatiiife <tundelawalb@gmail.com>
2023-07-03 15:08:45 +0100

    s

--- firstBranch.js
+++ firstBranch.js
@@ -1,7 +1,8 @@
-console.log('I love Hashcode\'s mentorship program');
-
-console.log('God bless Hashcode');
 
 let today='Monday';
 let tommorrow='Tuesday';
 let weather='sunny';
+
+console.log('I love Hashcode\'s mentorship program');
+
+console.log('God bless Hashcode');
